a6e9f23d5961ee0044eec5aa501a21e864cfe41d
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-05-31 16:18:07 -0400

    created post model

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,46 @@
 // used for posting new animals
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//creates our post model
+class Post extends Model {}
+
+// create fields/columns for post model
+Post.init(
+    {
+        //defines post schema
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        // tie to employee table because employee will be the one to post
+        employee_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'employee',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
