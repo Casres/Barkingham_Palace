ae749116525e31ba9bb3c4b5108baf37ebfe2d52
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-06-02 19:36:41 -0400

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,16 @@
 // import the sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-require('dotenv').congif();
+require('dotenv').config();
 
-//heroku env setup
-let sequelize;
 
-if(process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = process.env.JAWSBD_URL
+        ? new Sequelize(process.env.JAWSBD_URL)
+        : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
         // may have to change to 127.0.0.1
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
     });
-}
 
 module.exports = sequelize;
