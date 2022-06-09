bd92991634309b7769cfb9d5b3a434dbd0226ba3
Casres <christian@casillas.tech>
2022-06-09 17:25:09 -0400

    backend routes

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require("express");
-// const sequelize = require('./config/connection');
-// const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 // const routes = require('./controllers');
 const apiRoutes = require("./routes/apiRoutes");
 const db = require("./db/connection");
-// const hbs = exphbs.create({});
-// const session = require('express.session');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({});
+const session = require('express.session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//     secret: '',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// }
+const sess = {
+    secret: '',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
 
 const app = express();
 const PORT = process.env.PORT || 3001;
