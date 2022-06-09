c69f32ef10dc18888ace91442f825d62118dde79
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-06-09 17:19:26 -0400

    switched file layout

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const session = require('express.session');
+// const session = require('express.session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
-    secret: 'secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-}
+// const sess = {
+//     secret: 'secret',
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// }
 
 // middleware
-app.use(session(sess));
+// app.use(session(sess));
 
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //tuurn on routes
-app.use(require('.controllers/'));
+// app.use(require('.controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force:false }).then(() => {
