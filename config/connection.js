db50e0391906f0bc34ed654c054b3f0d4424ed24
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-06-11 13:43:13 -0400

    tinkering with handlebars

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,19 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 
-const sequelize = process.env.JAWSBD_URL
-        ? new Sequelize(process.env.JAWSBD_URL)
-        : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-        // may have to change to 127.0.0.1
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    });
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    port: 3306
+  });
+
+// const sequelize = process.env.JAWSBD_URL
+//     ? new Sequelize(process.env.JAWSBD_URL)
+//     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//     // may have to change to 127.0.0.1
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+// });
 
 module.exports = sequelize;
