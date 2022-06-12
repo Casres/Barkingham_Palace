4e10a6277192421d05c9f125de6cbc85c57301f4
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-06-11 23:22:06 -0400

    updated main and homepage

--- server.js
+++ server.js
@@ -24,15 +24,6 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//     secret: 'secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// }
 
 // middleware
 // app.use(session(sess));
