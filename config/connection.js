b4ef5e621ea458076864e41b8f88241df2e77dff
Casres <christian@casillas.tech>
2022-06-12 00:26:07 -0400

    SERVER ONLINE

--- config/connection.js
+++ config/connection.js
@@ -8,15 +8,5 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306
-  });
-
-// const sequelize = process.env.JAWSBD_URL
-//     ? new Sequelize(process.env.JAWSBD_URL)
-//     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-//     // may have to change to 127.0.0.1
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-// });
-
+});
 module.exports = sequelize;
